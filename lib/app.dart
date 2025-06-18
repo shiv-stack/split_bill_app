eed74be53828ab08950ad3d1bbb023b3b42865fb
Kunal <shivamgeu7@gmail.com>
2025-06-18 11:05:01 +0530

    user session fucntionality

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
 import 'blocs/auth/auth_bloc.dart';
-import 'screens/welcome_screen.dart';
 import 'screens/login_screen.dart';
 import 'screens/signup_screen.dart';
 import 'screens/homescreen.dart';
+import 'screens/welcome_screen.dart';
 
 class SplitBillApp extends StatelessWidget {
   const SplitBillApp({super.key});
@@ -16,12 +18,24 @@ class SplitBillApp extends StatelessWidget {
       child: MaterialApp(
         title: 'Split Bill App',
         debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primarySwatch: Colors.teal,
+        theme: ThemeData(primarySwatch: Colors.teal),
+        home: FutureBuilder<User?>(
+          future: Future.value(FirebaseAuth.instance.currentUser),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Scaffold(
+                body: Center(child: CircularProgressIndicator()),
+              );
+            } else {
+              if (snapshot.data != null) {
+                return const HomeScreen(); // User is logged in
+              } else {
+                return const WelcomeScreen(); // User not logged in
+              }
+            }
+          },
         ),
-        initialRoute: '/',
         routes: {
-          '/': (context) => const WelcomeScreen(),
           '/login': (context) => const LoginScreen(),
           '/signup': (context) => const SignupScreen(),
           '/home': (context) => const HomeScreen(),
